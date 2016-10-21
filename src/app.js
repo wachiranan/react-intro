b8cdfa3bd93b0a2ada3fc959632f4be4fff3ba72
wachiranan <ming-club@hotmail.com>
2016-10-21 21:28:15 +0700

    edit but forgot commit

--- src/app.js
+++ src/app.js
@@ -1,10 +1,61 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const title = 'My React App'
+const SearchForm = () => {
+      return (
+        <form>
+            <input type="text" />
+            <button type="submit">search</button>
+        </form>
+    )
+}
 
-const App =() => {
-    return <div>My React App</div>
+const Header =(props) => (
+    <header>
+        <h1>{props.title}</h1>
+        <SearchForm />
+    </header>
+)
+
+const Items = (props) => {
+    console.log(props.items)
+    return (
+        <ul>
+            {props.items.map(item => (
+                <li>{item}</li>)
+            )}
+        </ul>
+    )
+}
+
+const Content = (props) => (
+    <section>
+        <p>{props.description}</p>
+        <Items items={props.items}/>
+    </section>
+)
+
+const AppWithoutDescription = () => (
+    <Header title="No description here" />
+)
+
+const App = () => {
+    const appTitle = 'Fronttechs: React'
+    const description = 'This is a simple react application'
+    const items = [
+        "Oliver",
+        "Tobey",
+        "Charlie",
+        "Lacky",
+        "Poyo"
+    ]
+    return (
+    <section>
+        <Header title={appTitle}/>
+        <Content description={description}
+                 items={items}/>
+    </section>
+    )
 }
 
 const element = document.getElementById('app')
